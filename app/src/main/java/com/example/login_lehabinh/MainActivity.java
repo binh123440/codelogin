6e2742af45cc2ee0b67b6f38286a9a23ab9b39b5
Le Ha Binh <binhgolden123@gmail.com>
2023-09-25 15:15:46 +0700

    upcodelan1

---
+++ app/src/main/java/com/example/login_lehabinh/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.login_lehabinh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lehabinh_login);
+    }
+}
